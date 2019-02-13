428fd90b2fd1567cf405e812aee9cf7fa446e6e4
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-13 02:35:25 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(array)
+  counter = 0 
+  new_collection = []
+  
+  while counter < array.length
+    new_collection << yield(array[counter])
+    counter += 1
+  end
+  new_collection
+end
 
